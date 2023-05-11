482c5bd20cde0d789bfc519a0685620286110fcd
Nysa Clark <Nysa.Clark@west-mec.org>
2023-05-11 14:31:46 -0700

    boiler-plate first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// import the styles and components you're going to use
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css"
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
+
 function App() {
-  const [count, setCount] = useState(0)
+  // Messages State will go here
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div /* styles will go here */ >
+        {/* MainContainer will start here */}
+          {/* ChatContainer will start here */}
+            {/* MessageList will start here */}
+              {/* message array will be mapped here */}
+            {/* MessageList will stop here */}
+          {/* Chat Container will stop here */}
+        {/* MainContainer will stop here */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
